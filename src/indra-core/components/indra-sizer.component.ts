b7e80e05d98b7c3e67937deeee41ad90f48d327f
formacion <madelatorre89@gmail.com>
2018-10-23 16:55:11 +0200

    Sizer

--- src/indra-core/components/indra-sizer.component.ts
+++ src/indra-core/components/indra-sizer.component.ts
@@ -13,12 +13,13 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 export class IndraSizerComponent {
 
-  @Input() size: number | string;
+  @Input()  size: number | string;
   @Output() sizeChange = new EventEmitter<number>();
-  dec() {this.resize(-1); }
-  inc() {this.resize(+1); }
-  resize(delta: number ) {
-    this.size = Math.min(40, Math.max(8, delta));
+  dec() { this.resize(-1); }
+  inc() { this.resize(+1); }
+  resize(delta: number) {
+    this.size = Math.min(40, Math.max(8, +this.size + delta));
+    this.sizeChange.emit(this.size);
   }
 
 }
